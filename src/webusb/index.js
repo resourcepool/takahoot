0e56d01881da7bb5d7b311473879477ae6224b60
loic ortola <contact@loicortola.com>
2020-02-06 11:04:08 +0100

    Refactored simulator

--- src/webusb/index.js
+++ src/webusb/index.js
@@ -3,6 +3,8 @@ var serial = {};
 (function() {
     'use strict';
 
+
+
     serial.getPorts = function() {
         return navigator.usb.getDevices().then(devices => {
             return devices.map(device => new serial.Port(device));
@@ -76,12 +78,48 @@ var serial = {};
 
 let port;
 
+function hexStringToByte(byteAsHexString) {
+  return parseInt(byteAsHexString, 16);
+}
+
+/**
+ * Converts a Hex String or a Pretty Hex String to an array of bytes (Uint8Array)
+ * @param hexStr
+ * @returns {Uint8Array}
+ */
+function hexStringToBytes(hexStr) {
+  const SEPARATORS = new RegExp('[-:]', 'g');
+  let str = hexStr.replace(SEPARATORS,'');
+  let size = str.length / 2;
+  let bytes = new Uint8Array(size + 2);
+  for (let i = 0; i < size; ++i) {
+    bytes[i] = hexStringToByte(str.substr(2 *  i, 2));
+  }
+  bytes[size] = 0x0D;
+  bytes[size + 1] = 0x0A;
+  return bytes;
+}
+
+function byteToHexString(byte) {
+  return (((byte & 0xF0) >> 4).toString(16) + (byte & 0x0F).toString(16)).toLowerCase();
+}
+
+function bytesToPrettyHexString(bytes) {
+  let hex = '';
+  for (let byte of bytes.values()) {
+    if (hex.length > 0) {
+      hex += ':';
+    }
+    hex += byteToHexString(byte);
+  }
+  return hex.toLowerCase();
+}
+
 function connect() {
     port.connect().then(() => {
         port.onReceive = data => {
-            let textDecoder = new TextDecoder();
-            console.log("Received:", textDecoder.decode(data));
-            document.getElementById('output').value += textDecoder.decode(data);
+            console.log("Received:", bytesToPrettyHexString(data));
+            document.getElementById('output').value += bytesToPrettyHexString(data);
         };
         port.onReceiveError = error => {
             console.error(error);
@@ -92,15 +130,15 @@ function connect() {
 }
 
 function send(string) {
+    string = string.trim();
     console.log("sending to serial:" + string.length);
     if (string.length === 0)
         return;
     console.log("sending to serial: [" + string +"]\n");
-
-    let view = new TextEncoder('utf-8').encode(string);
-    console.log(view);
+    const bytes = hexStringToBytes(string);
+    console.log(bytes);
     if (port) {
-        port.send(view);
+        port.send(bytes);
     }
 }
 
@@ -116,5 +154,41 @@ window.onload = () => {
     document.querySelector("#submit").onclick = () => {
         let source = document.querySelector("#editor").value;
         send(source);
+    }
+
+    document.querySelector("#sendConnect").onclick = () => {
+        document.querySelector("#editor").innerHTML = "30";
+    }
+
+    document.querySelector("#connectAndCalibrate").onclick = () => {
+        document.querySelector("#editor").innerHTML = "3100310131023103";
+    }
+
+    document.querySelector("#changeTolerance").onclick = () => {
+        document.querySelector("#editor").innerHTML = "32XXYY";
+    }
+
+    document.querySelector("#enableTarget").onclick = () => {
+        document.querySelector("#editor").innerHTML = "34XX";
+    }
+
+    document.querySelector("#disableTarget").onclick = () => {
+        document.querySelector("#editor").innerHTML = "35XX";
+    }
+
+    document.querySelector("#disableTargetAndBlink").onclick = () => {
+        document.querySelector("#editor").innerHTML = "36XX";
+    }
+
+    document.querySelector("#disableTargetsAndBlink").onclick = () => {
+        document.querySelector("#editor").innerHTML = "37";
+    }
+
+    document.querySelector("#getState").onclick = () => {
+        document.querySelector("#editor").innerHTML = "38";
+    }
+
+    document.querySelector("#reset").onclick = () => {
+        document.querySelector("#editor").innerHTML = "3F";
     }
 };
