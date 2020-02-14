3a89ce107eee123fc87d06e1fca2201925e1fd94
Alex Nunes <anunesse@takima.fr>
2020-02-14 17:44:18 +0100

    Remove takahoot electron client

--- src/kahoot-service/kahoot.js/index.js
+++
@@ -1,4 +0,0 @@
-//Custom version of Kahoot.js to support web sockets in electron
-var Client = require('./src/kahoot.js');
-
-module.exports = Client;
--- test-kahoot.js
+++
@@ -1,7 +0,0 @@
-const KahootSession = require("./src/kahoot-service/KahootSession");
-
-const kahootSession = new KahootSession("7907205", "Alex", 1);
-
-kahootSession.joinPromise.then(() => {
-    console.info("Kahoot session joined !");
-});
