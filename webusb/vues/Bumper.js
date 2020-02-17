25d2527bea9dece44fa87ae80abce8775986a584
Alex Nunes <anunesse@takima.fr>
2020-02-17 11:48:23 +0100

    Add bumper

---
+++ webusb/vues/Bumper.js
@@ -0,0 +1,14 @@
+import { el } from "/node_modules/redom/dist/redom.es.js";
+import BumperActions from "./BumperActions.js";
+
+const BUMPER_TYPES = ["is-danger", "is-warning", "is-info", "is-success"];
+
+export default class Bumper {
+    constructor(targetIndex, bumperIndex) {
+        this.el = el("article.tile.is-child.notification.box." + BUMPER_TYPES[bumperIndex],
+            [
+                new BumperActions(targetIndex, bumperIndex)
+            ]
+        )
+    }
+}
--- webusb/vues/Target.js
+++ webusb/vues/Target.js
@@ -1,6 +1,6 @@
 import { el } from "/node_modules/redom/dist/redom.es.js";
 import TargetActions from "./TargetActions.js";
-import BumperActions from "./BumperActions.js";
+import Bumper from "./Bumper.js";
 
 export default class Target {
     constructor(targetIndex) {
@@ -13,30 +13,14 @@ export default class Target {
                         [
                             el(".tile.is-parent.is-vertical",
                                 [
-                                    el("article.tile.is-child.notification.is-danger.box",
-                                        [
-                                            new BumperActions(targetIndex, 0)
-                                        ]
-                                    ),
-                                    el("article.tile.is-child.notification.is-warning.box",
-                                        [
-                                            new BumperActions(targetIndex, 1)
-                                        ]
-                                    ),
+                                    new Bumper(targetIndex, 0),
+                                    new Bumper(targetIndex, 1),
                                 ]
                             ),
                             el(".tile.is-parent.is-vertical",
                                 [
-                                    el("article.tile.is-child.notification.is-info.box",
-                                        [
-                                            new BumperActions(targetIndex, 2)
-                                        ]
-                                    ),
-                                    el("article.tile.is-child.notification.is-success.box",
-                                        [
-                                            new BumperActions(targetIndex, 3)
-                                        ]
-                                    ),
+                                    new Bumper(targetIndex, 2),
+                                    new Bumper(targetIndex, 3),
                                 ]
                             )
                         ])
