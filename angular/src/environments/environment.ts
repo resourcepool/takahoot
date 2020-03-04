7ec0a87e2c7d6ed69fbc7a0e3afa9293cdd10e3e
loic ortola <contact@loicortola.com>
2020-03-04 17:08:46 +0100

    Added angular boilerplate

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAx0cIDJD8oC-E_XbIPj_ewau9EChSvBzs",
+    authDomain: "takima-takahoot.firebaseapp.com",
+    databaseURL: "https://takima-takahoot.firebaseio.com",
+    projectId: "takima-takahoot",
+    storageBucket: "takima-takahoot.appspot.com",
+    messagingSenderId: "948003460395",
+    appId: "1:948003460395:web:46df1d0fc045acef34e16e"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
